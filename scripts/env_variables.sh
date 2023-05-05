69780560dab91a072972aeab6407dc5df33349e3
Petar Svetlinov <psvetlinovpe@vmware.com>
2023-05-05 18:45:20 +0300

    Added scripts for setting env variables and go fmt checks

---
+++ scripts/env_variables.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# VCDA provider configuration
+export VCDA_IP=''
+export LOCAL_USER=''
+export LOCAL_PASSWORD=''
+
+# vSphere server and credentials
+export VSPHERE_SERVER=''
+export VSPHERE_USER=root
+export VSPHERE_PASSWORD=''
+
+# The managed object ID of the datacenter in which the appliances are placed 
+export DC_ID=''
+
+# Appliances VM names
+export CLOUD_VM_NAME=''
+export MANAGER_VM_NAME=''
+export REPLICATOR_VM_NAME=''
+export TUNNEL_VM_NAME=''
+
+# Passwords
+export CURRENT_PASSWORD=''
+export NEW_PASSWORD=''
+export ROOT_PASSWORD=''
+
+# Cloud Director/Vcenter Replication Manager configuration
+export LICENSE_KEY=''
+export VCD_USERNAME=''
+export VCD_PASSWORD=''
+export VCD_ADDRESS=''
+
+# Lookup service details
+export LS_ADDRESS=''
+export SSO_USER=''
+export SSO_PASSWORD=''
+
+# Services endpoints
+export MANAGER_ADDRESS=''
+export REPLICATOR_ADDRESS=''
+export TUNNEL_ADDRESS=''
